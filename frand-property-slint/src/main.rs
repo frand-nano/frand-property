e4aea4a7e8ce10d59da2bc62d13ba1dcc5d5fdc2
frand.nano <frand.nano@gmail.com>
2025-12-14 13:07:43 +0900

    덧셈 예제 UI와 모델 추가
      1. 시작 화면에 Adder UI 배치
          - X/Y 증감 버튼과 합계 텍스트 연결
      2. Adder 모델 연동
          - Rust 태스크에서 x·y 합계를 계산해 전송

--- frand-property-slint/src/main.rs
+++ frand-property-slint/src/main.rs
@@ -10,10 +10,19 @@ slint_model! {
     }
 }
 
+slint_model! {
+    AdderModel: AdderData {
+        in x: i32,
+        in y: i32,
+        out sum: i32,
+    }
+}
+
 #[tokio::main]
 async fn main() {
     let window = MainWindow::new().unwrap(); // TODO: 에러 처리
     let screen_model = ScreenModel::<MainWindow>::new(&window);
+    let adder_model = AdderModel::<MainWindow>::new(&window);
 
     tokio::spawn(async move {
         let mut confirm_start = screen_model.confirm_start.receiver().clone();
@@ -27,6 +36,26 @@ async fn main() {
             screen_model.current_screen.send(ScreenVariant::Pay);
 
             cancel_pay.changed().await;
+        }
+    });
+
+    tokio::spawn(async move {
+        let mut x_rx = adder_model.x.receiver().clone();
+        let mut y_rx = adder_model.y.receiver().clone();
+
+        let mut x = 0;
+        let mut y = 0;
+
+        loop {
+            tokio::select! {
+                new_x = x_rx.changed() => {
+                    x = new_x;
+                }
+                new_y = y_rx.changed() => {
+                    y = new_y;
+                }
+            }
+            adder_model.sum.send(x + y);
         }
     });
 
